8e7f2bf4bd9341c2a11078bcc0d9c58bcce551ab
abschill <ajay198810@gmail.com>
2022-07-08 21:15:43 -0600

    chore: close #8@0.3.2

---
+++ test/main.test.js
@@ -0,0 +1,11 @@
+const { expect } = require('expect');
+const {
+	it,
+	describe
+} = require('mocha');
+
+describe('base', () => {
+	it('test runner placeholder works', () => {
+		expect(1).toBe(1);
+	});
+});
